3938c25ea749426250db4be3b6b7c91a24137e89
Yohannes Masterous <yohannes.masterous@gmail.com>
2023-03-10 10:14:10 +0700

    Update MainActivity.java

--- app/src/main/java/masterous/si6a/kamusku/activities/MainActivity.java
+++ app/src/main/java/masterous/si6a/kamusku/activities/MainActivity.java
@@ -1,6 +1,7 @@
 package masterous.si6a.kamusku.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
 import android.view.View;
@@ -10,13 +11,19 @@ import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import masterous.si6a.kamusku.R;
+import masterous.si6a.kamusku.adapters.KamusViewAdapter;
 import masterous.si6a.kamusku.databases.KamusHelper;
 import masterous.si6a.kamusku.databinding.ActivityMainBinding;
+import masterous.si6a.kamusku.models.Kamus;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
     private KamusHelper kamusHelper;
+    private KamusViewAdapter kamusViewAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +31,11 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        kamusHelper = new KamusHelper(this);
+        kamusViewAdapter = new KamusViewAdapter();
+        binding.rvKamus.setLayoutManager(new LinearLayoutManager(this));
+        binding.rvKamus.setAdapter(kamusViewAdapter);
 
         binding.btnSearch.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,7 +45,15 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void getAllData() {
+    @Override
+    protected void onResume() {
+        super.onResume();
+        getAllData();
+    }
 
+    private void getAllData() {
+        kamusHelper.open();
+        ArrayList<Kamus> kamus = kamusHelper.getAllData();
+        kamusViewAdapter.setData(kamus);
     }
 }
