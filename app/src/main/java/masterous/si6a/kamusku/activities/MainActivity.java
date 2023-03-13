d5a6c9233a27a6305e8ac5e2c445943f2783c908
CristianChandra <chandradinta@mhs.mdp.ac.id>
2023-03-13 10:12:26 +0700

    Update

--- app/src/main/java/masterous/si6a/kamusku/activities/MainActivity.java
+++ app/src/main/java/masterous/si6a/kamusku/activities/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -41,6 +42,15 @@ public class MainActivity extends AppCompatActivity {
         binding.btnSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String strSearch = binding.etSearch.getText().toString();
+
+                if (TextUtils.isEmpty(strSearch)) {
+                    getAllData();
+                }else {
+                    kamusHelper.open();
+                    ArrayList<Kamus> kamus = kamusHelper.getAllDataByTitle(strSearch);
+                    kamusViewAdapter.setData(kamus);
+                }
 
             }
         });
